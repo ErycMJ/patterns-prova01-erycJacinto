6ccb2f61c4bb856f8d4d4d7c6a2935315062cb21
= <=>
2025-09-18 19:33:18 -0300

    feat: resolution of the exercises

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,75 @@
-console.log("Hello, World! 001");
+class Button {
+    render() { }
+}
+
+class Window {
+    render() { }
+}
+
+class LightButton extends Button {
+    render() {
+        return "Botão branco criado";
+    }
+}
+
+class DarkButton extends Button {
+    render() {
+        return "Botão preto criado";
+    }
+}
+
+class LightWindow extends Window {
+    render() {
+        return "Janela clara aberta";
+    }
+}
+
+class DarkWindow extends Window {
+    render() {
+        return "Janela escura aberta";
+    }
+}
+
+class UIThemeFactory {
+    createButton() { }
+    createWindow() { }
+}
+
+class LightThemeFactory extends UIThemeFactory {
+    createButton() {
+        return new LightButton();
+    }
+    createWindow() {
+        return new LightWindow();
+    }
+}
+
+class DarkThemeFactory extends UIThemeFactory {
+    createButton() {
+        return new DarkButton();
+    }
+    createWindow() {
+        return new DarkWindow();
+    }
+}
+
+function getUIComponents(theme) {
+    let factory;
+
+    if (theme === "light") {
+        factory = new LightThemeFactory();
+    } else if (theme === "dark") {
+        factory = new DarkThemeFactory();
+    }
+
+    const button = factory.createButton();
+    const windowUI = factory.createWindow();
+
+    return { button, windowUI };
+}
+
+const theme = "dark";
+const { button, windowUI } = getUIComponents(theme);
+
+console.log(button.render());
+console.log(windowUI.render());
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,83 @@
-console.log("Hello, World! 002");
+class Lanche {
+    constructor(pao, carne, queijo, salada, molho) {
+        this.pao = pao;
+        this.carne = carne;
+        this.queijo = queijo;
+        this.salada = salada;
+        this.molho = molho;
+    }
+
+    show() {
+        console.log("Lanche:", {
+            pao: this.pao,
+            carne: this.carne,
+            queijo: this.queijo,
+            salada: this.salada,
+            molho: this.molho,
+        });
+    }
+}
+
+class LancheBuilder {
+    constructor() {
+        this.pao = false;
+        this.carne = false;
+        this.queijo = false;
+        this.salada = false;
+        this.molho = false;
+    }
+
+    addPao() {
+        this.pao = true;
+        return this;
+    }
+
+    addCarne() {
+        this.carne = true;
+        return this;
+    }
+
+    addQueijo() {
+        this.queijo = true;
+        return this;
+    }
+
+    addSalada() {
+        this.salada = true;
+        return this;
+    }
+
+    addMolho() {
+        this.molho = true;
+        return this;
+    }
+
+    build() {
+        return new Lanche(this.pao, this.carne, this.queijo, this.salada, this.molho);
+    }
+}
+
+class Diretor {
+    static criarLancheSimples() {
+        return new LancheBuilder()
+            .addPao()
+            .addCarne()
+            .build();
+    }
+
+    static criarLancheCompleto() {
+        return new LancheBuilder()
+            .addPao()
+            .addCarne()
+            .addQueijo()
+            .addSalada()
+            .addMolho()
+            .build();
+    }
+}
+
+const lancheSimples = Diretor.criarLancheSimples();
+const lancheCompleto = Diretor.criarLancheCompleto();
+
+lancheSimples.show();
+lancheCompleto.show();
